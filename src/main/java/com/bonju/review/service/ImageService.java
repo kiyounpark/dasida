705b07b40e2e307e6f6b48763f652086dca86b34
박기윤 <qq8918@naver.com>
2025-02-08 22:46:24 +0900

    image Service 삭제

--- src/main/java/com/bonju/review/service/ImageService.java
+++
@@ -1,9 +0,0 @@
-package com.bonju.review.service;
-
-import com.bonju.review.dto.ImageRequestDto;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImageService {
-
-    String getImageUrl(ImageRequestDto imageRequestDto);
-}
