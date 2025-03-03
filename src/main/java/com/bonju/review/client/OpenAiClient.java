91e552bc92f732063d6d3cc29bffda8bc5d79cdf
박기윤 <qq8918@naver.com>
2025-03-03 15:22:30 +0900

    update OpenAiClient CreateQuiz 오버로딩 퀴즈 추가 add test.html 임시 데이터 설정

--- src/main/java/com/bonju/review/client/OpenAiClient.java
+++ src/main/java/com/bonju/review/client/OpenAiClient.java
@@ -4,7 +4,12 @@ import com.bonju.review.exception.exception.OpenAiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
+
+import java.net.MalformedURLException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -36,6 +41,32 @@ public class OpenAiClient {
             return response;
         } catch (OpenAiException e) {
             log.error("OpenAI API 호출 실패: prompt={}, 에러 메시지={}", prompt, e.getMessage(), e); // 에러 로깅
+            throw new OpenAiException("OpenAI API가 응답을 반환하는데 실패하였습니다.");
+        }
+    }
+
+    private String createQuiz(String prompt, List<String> imageUrls) {
+        try {
+            log.debug("ChatClient 호출 시작: prompt={}", prompt);
+            String response = chatClient.prompt()
+                    .user(userSpec -> {
+                        // HTML 프롬프트 설정
+                        userSpec.text(prompt);
+                        // imageUrls 리스트에 값이 없으면 forEach는 실행되지 않습니다.
+                        imageUrls.forEach(url -> {
+                            try {
+                                userSpec.media(MimeTypeUtils.IMAGE_JPEG, new UrlResource(url));
+                            } catch (MalformedURLException e) {
+                                log.error("잘못된 이미지 URL: {}", url, e);
+                            }
+                        });
+                    })
+                    .call()
+                    .content();
+            log.debug("ChatClient 호출 성공: 응답={}", response);
+            return response;
+        } catch (OpenAiException e) {
+            log.error("OpenAI API 호출 실패: prompt={}, 에러 메시지={}", prompt, e.getMessage(), e);
             throw new OpenAiException("OpenAI API가 응답을 반환하는데 실패하였습니다.");
         }
     }
