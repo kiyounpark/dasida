792d89da632b2faf11663de975ada6e7ce704b8a
박기윤 <qq8918@naver.com>
2025-03-09 15:33:37 +0900

    dto 수정

--- src/main/java/com/bonju/review/wrong_answer_note/dto/WrongAnswerGroupResponseDto.java
+++ src/main/java/com/bonju/review/wrong_answer_note/dto/WrongAnswerGroupResponseDto.java
@@ -11,6 +11,6 @@ public class WrongAnswerGroupResponseDto {
 
     private Long quizId;
     private String quizText;         // 퀴즈 질문(문제)
-    private String correctAnswer;    // 퀴즈 정답
+    private String quizAnswer;    // 퀴즈 정답
     private List<WrongAnswerResponseDto> answers; // 사용자의 여러 오답(답변) 목록
 }
