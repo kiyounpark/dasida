b052113f114121ddceab8b255c9ac432d0cca2cf
박기윤 <qq8918@naver.com>
2025-03-18 13:48:55 +0900

    update 통합 테스트를 위해 필드 추가

--- src/test/java/com/bonju/review/BaseTest.java
+++ src/test/java/com/bonju/review/BaseTest.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -17,4 +22,7 @@ public abstract class BaseTest {
 
     @Autowired
     protected OAuth2ClientProperties oAuth2ClientProperties;
+
+    @MockitoBean
+    protected OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
 }
