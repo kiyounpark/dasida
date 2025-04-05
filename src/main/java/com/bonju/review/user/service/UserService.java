334948edbfbbae13bd532e9c92efd7f60c7f54b1
박기윤 <qq8918@naver.com>
2025-04-05 20:43:59 +0900

    ADD UserService에 카카오 로그인 성공시 처리 로직 추

--- src/main/java/com/bonju/review/user/service/UserService.java
+++ src/main/java/com/bonju/review/user/service/UserService.java
@@ -3,10 +3,14 @@ package com.bonju.review.user.service;
 import com.bonju.review.user.helper.AuthenticationHelper;
 import com.bonju.review.user.entity.User;
 import com.bonju.review.user.repository.UserRepository;
+import com.bonju.review.user.vo.KakaoUser;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +24,25 @@ public class UserService {
         return userRepository.findByKaKaoId(kakaoId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "사용자를 찾을 수 없습니다."));
     }
+
+    @Transactional
+    public void upsertUserByKakaoId(KakaoUser kakaoUser) {
+        String kakaoId = kakaoUser.id();
+        String nickname = kakaoUser.properties().nickname();
+
+        Optional<User> optionalUser = userRepository.findByKaKaoId(kakaoId);
+
+        if (optionalUser.isEmpty()) {
+            User newUser = User.builder()
+                    .kakaoId(kakaoId)
+                    .nickname(nickname).build();
+
+            userRepository.save(newUser);
+            return;
+        }
+
+        User existingUser = optionalUser.get();
+        existingUser.updateUserNickname(nickname);
+    }
+
 }
