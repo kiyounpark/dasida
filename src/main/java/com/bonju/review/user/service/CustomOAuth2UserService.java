2816bd196ba7abedc8250b6f86fbc4a40a2f955d
박기윤 <qq8918@naver.com>
2025-04-05 20:41:34 +0900

    refactor 유저 db 저장 코드 UserService 이동 후 불러옴

--- src/main/java/com/bonju/review/user/service/CustomOAuth2UserService.java
+++ src/main/java/com/bonju/review/user/service/CustomOAuth2UserService.java
@@ -1,16 +1,12 @@
 package com.bonju.review.user.service;
-
-import com.bonju.review.user.dto.KakaoUserInfoDto;
-import com.bonju.review.user.entity.User;
-import com.bonju.review.user.mapper.KakaoUserParser;
-import com.bonju.review.user.repository.UserRepository;
+import com.bonju.review.user.vo.KakaoUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
@@ -18,35 +14,17 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
+    private final ObjectMapper objectMapper;
 
     @Override
-    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        // 카카오 정보 가져오기
         Map<String, Object> attributes = oAuth2User.getAttributes();
-        KakaoUserInfoDto kakaoUserInfoDto = KakaoUserParser.parseKakaoUserInfo(attributes);
 
-        // 저장 또는 업데이트 하기
-        saveOrUpdateUser(kakaoUserInfoDto);
+        KakaoUser kakaoUser = objectMapper.convertValue(attributes, KakaoUser.class);
+        userService.upsertUserByKakaoId(kakaoUser);
 
         return oAuth2User;
-    }
-
-    @Transactional
-    private void saveOrUpdateUser(KakaoUserInfoDto kakaoUserInfoDto) {
-        userRepository.findByKaKaoId(kakaoUserInfoDto.getId())
-                .map(existingUser -> {
-                    // 유저 정보가 있다면 닉네임만 업데이트
-                    existingUser.setNickname(kakaoUserInfoDto.getNickname());
-                    return existingUser;
-                })
-                .orElseGet(() -> {
-                    // 새로운 유저 생성
-                    User newUser = new User(kakaoUserInfoDto.getId(), kakaoUserInfoDto.getNickname());
-                    return userRepository.save(newUser);
-                });
     }
 }
