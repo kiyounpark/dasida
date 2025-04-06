f72cc08c80f01f7526c9eacb549dd98629e269c1
박기윤 <qq8918@naver.com>
2025-04-06 20:03:16 +0900

    REFACTORING 빈 충돌 방지 위해 이름 수

--- src/main/java/com/bonju/review/knowledge_quiz/service/KnowledgeQuizServiceImpl.java
+++ src/main/java/com/bonju/review/knowledge_quiz/service/KnowledgeQuizServiceImpl.java
@@ -2,7 +2,7 @@ package com.bonju.review.knowledge_quiz.service;
 
 import com.bonju.review.knowledge.dto.KnowledgeRequestDto;
 import com.bonju.review.knowledge.entity.Knowledge;
-import com.bonju.review.knowledge.service.register.KnowledgeService;
+import com.bonju.review.knowledge.service.register.KnowledgeRegisterService;
 import com.bonju.review.quiz.service.register.QuizRegisterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,14 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class KnowledgeQuizServiceImpl implements KnowledgeQuizService {
 
-    private final KnowledgeService knowledgeService;
+    private final KnowledgeRegisterService knowledgeRegisterService;
     private final QuizRegisterService quizService;
 
 
     @Override
     @Transactional
     public void registerKnowledgeAndQuiz(KnowledgeRequestDto knowledgeRequestDto) {
-        Knowledge knowledge = knowledgeService.registerKnowledge(knowledgeRequestDto);
+        Knowledge knowledge = knowledgeRegisterService.registerKnowledge(knowledgeRequestDto);
         quizService.createQuiz(knowledge);
     }
 }
