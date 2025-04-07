643faeb2a794d19e6533b85a8f62d4a517fc9057
박기윤 <qq8918@naver.com>
2025-04-07 23:12:04 +0900

    UPDATE DayKnowledgeResponseDto 생성자로 생성 못하고 빌드로만 생성 가능하게 변경

--- src/main/java/com/bonju/review/knowledge/dto/DayKnowledgeResponseDto.java
+++ src/main/java/com/bonju/review/knowledge/dto/DayKnowledgeResponseDto.java
@@ -1,13 +1,21 @@
 package com.bonju.review.knowledge.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class DayKnowledgeResponseDto {
+
     private final int dayType; // "0일차", "3일차", "7일차", "30일차"
     private final Long id;
     private final String title;
     private final String content;
+
+    @Builder
+    private DayKnowledgeResponseDto(int dayType, Long id, String title, String content) {
+        this.dayType = dayType;
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
