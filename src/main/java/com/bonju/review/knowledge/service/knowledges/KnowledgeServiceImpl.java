b047ca70c0a813d9f5263de2a1c76b15a9727708
박기윤 <qq8918@naver.com>
2025-04-07 12:51:20 +0900

    REFACTORING 일급 컬렉션 분리 및 적

---
+++ src/main/java/com/bonju/review/knowledge/service/knowledges/KnowledgeServiceImpl.java
@@ -0,0 +1,30 @@
+// KnowledgeServiceImpl.java
+package com.bonju.review.knowledge.service.knowledges;
+
+import com.bonju.review.knowledge.converter.MarkdownConverter;
+import com.bonju.review.knowledge.dto.DayKnowledgeResponseDto;
+import com.bonju.review.knowledge.repository.knowledges.KnowledgesRepository;
+import com.bonju.review.knowledge.vo.DayKnowledgeResponses;
+import com.bonju.review.user.entity.User;
+import com.bonju.review.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class KnowledgeServiceImpl implements KnowledgesService {
+
+    private final KnowledgesRepository knowledgesRepository;
+    private final UserService userService;
+    private final MarkdownConverter markdownConverter;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<DayKnowledgeResponseDto> getAllDayKnowledges() {
+        User user = userService.findUser();
+        DayKnowledgeResponses responses = DayKnowledgeResponses.from(user, knowledgesRepository, markdownConverter);
+        return responses.asList();
+    }
+}
