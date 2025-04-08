83f775b561643222a59c9a7fe77871e1214b7141
박기윤 <qq8918@naver.com>
2025-04-08 15:39:01 +0900

    ADD BaseTest 파일에 필요한 객체 추가

--- src/test/java/com/bonju/review/BaseTest.java
+++ src/test/java/com/bonju/review/BaseTest.java
@@ -1,9 +1,13 @@
 package com.bonju.review;
 
 import com.bonju.review.knowledge.converter.MarkdownConverter;
+import com.bonju.review.knowledge.repository.knowledges.KnowledgesRepository;
+import com.bonju.review.knowledge.repository.register.KnowledgeRegisterRepository;
+import com.bonju.review.knowledge.service.register.KnowledgeRegisterService;
 import com.bonju.review.user.repository.UserRepository;
 import com.bonju.review.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -40,4 +44,17 @@ public abstract class BaseTest {
 
     @Autowired
     protected MarkdownConverter markdownConverter;
+
+    @Autowired
+    protected KnowledgeRegisterService knowledgeRegisterService;
+
+    @Autowired
+    protected KnowledgesRepository knowledgesRepository;
+
+    @Autowired
+    protected KnowledgeRegisterRepository knowledgeRegisterRepository;
+
+    @Autowired
+    protected EntityManager entityManager;
+
 }
