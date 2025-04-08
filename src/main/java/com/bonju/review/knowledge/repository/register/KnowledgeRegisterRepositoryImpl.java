4853064ba58b0cff837cc9b33c9f1c240268f60d
박기윤 <qq8918@naver.com>
2025-04-08 15:40:35 +0900

    REFACTORING KnowledgeRepository -> KnowledgeRegisterRepository 로 변경

--- src/main/java/com/bonju/review/knowledge/repository/register/KnowledgeRegisterRepositoryImpl.java
+++ src/main/java/com/bonju/review/knowledge/repository/register/KnowledgeRegisterRepositoryImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class KnowledgeRepositoryImpl implements KnowledgeRepository {
+public class KnowledgeRegisterRepositoryImpl implements KnowledgeRegisterRepository {
 
     private final EntityManager em;
 
