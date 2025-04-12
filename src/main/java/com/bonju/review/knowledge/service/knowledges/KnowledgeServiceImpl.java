6a5fa60addeeef4fafa32823291f6b13397f89a8
박기윤 <qq8918@naver.com>
2025-04-12 16:58:07 +0900

    UPDATE 깔끔한 코드 처리를 위해 exception 코드 추가

--- src/main/java/com/bonju/review/knowledge/service/knowledges/KnowledgeServiceImpl.java
+++ src/main/java/com/bonju/review/knowledge/service/knowledges/KnowledgeServiceImpl.java
@@ -1,16 +1,20 @@
-// KnowledgeServiceImpl.java
 package com.bonju.review.knowledge.service.knowledges;
 
 import com.bonju.review.knowledge.converter.MarkdownConverter;
 import com.bonju.review.knowledge.dto.DayKnowledgeResponseDto;
+import com.bonju.review.knowledge.entity.Knowledge;
+import com.bonju.review.knowledge.exception.KnowledgeException;
 import com.bonju.review.knowledge.repository.knowledges.KnowledgesRepository;
-import com.bonju.review.knowledge.vo.DayKnowledgeResponses;
 import com.bonju.review.user.entity.User;
 import com.bonju.review.user.service.UserService;
+import com.bonju.review.util.enums.DayType;
 import com.google.common.collect.ImmutableList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +28,29 @@ public class KnowledgeServiceImpl implements KnowledgesService {
     @Transactional(readOnly = true)
     public ImmutableList<DayKnowledgeResponseDto> getAllDayKnowledges() {
         User user = userService.findUser();
-        DayKnowledgeResponses responses = DayKnowledgeResponses.from(user, knowledgesRepository, markdownConverter);
-        return responses.asImmutableList();
+        ImmutableList.Builder<DayKnowledgeResponseDto> dtoListBuilder = ImmutableList.builder();
+
+        for (DayType dayType : DayType.values()) {
+            dtoListBuilder.addAll(fetchDtosByDayType(user, dayType));
+        }
+
+        return dtoListBuilder.build();
+    }
+
+    private List<DayKnowledgeResponseDto> fetchDtosByDayType(User user, DayType dayType) {
+        try {
+            List<Knowledge> knowledgeList = knowledgesRepository.findKnowledgesByDaysAgo(user, dayType.getDaysAgo());
+
+            return knowledgeList.stream()
+                    .map(knowledge -> DayKnowledgeResponseDto.builder()
+                            .dayType(dayType.getDaysAgo())
+                            .id(knowledge.getId())
+                            .title(knowledge.getTitle())
+                            .content(markdownConverter.convertTruncatedParagraph(knowledge.getContent()))
+                            .build())
+                    .toList();
+        } catch (DataAccessException e) {
+            throw new KnowledgeException("지식 조회 중 오류가 발생했습니다: " + dayType.name(), e);
+        }
     }
 }
