ee0e78aa6423a4e06b72ab7231e72df3966faadf
박기윤 <qq8918@naver.com>
2025-04-12 16:58:07 +0900

    UPDATE 깔끔한 코드 처리를 위해 exception 코드 추가

---
+++ src/main/java/com/bonju/review/knowledge/exception/KnowledgeException.java
@@ -0,0 +1,8 @@
+package com.bonju.review.knowledge.exception;
+
+  public class KnowledgeException extends RuntimeException {
+    public KnowledgeException(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
+
