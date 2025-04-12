4fe3b124df34baf83a5d351091799a18cd1cf667
박기윤 <qq8918@naver.com>
2025-04-12 17:38:44 +0900

    UPDATE ADD DayQuizResponseDto field hint, answerLength 추가

--- src/main/java/com/bonju/review/quiz/dto/DayQuizResponseDto.java
+++ src/main/java/com/bonju/review/quiz/dto/DayQuizResponseDto.java
@@ -1,11 +1,16 @@
 package com.bonju.review.quiz.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 @Getter
 @AllArgsConstructor
+@Builder
 public class DayQuizResponseDto {
     private final int dayType;  // 내부적으로 Enum 보관
     private final Long quizId;
     private final String question;
+    private String hint;
+    private final int answerLength;
+
 }
