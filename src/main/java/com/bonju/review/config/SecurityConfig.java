6fb33b508f53bb1878912a2ae5dd0a933a32aef4
박기윤 <qq8918@naver.com>
2025-04-14 01:26:37 +0900

    UPDATE SecurityConfig 통

--- src/main/java/com/bonju/review/config/SecurityConfig.java
+++ src/main/java/com/bonju/review/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -16,7 +17,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @Configuration
 @RequiredArgsConstructor
 @Profile("local")
-public class LocalSecurityConfig {
+public class SecurityConfig {
 
     private final AuthenticationFailureHandler failureHandler;
     private final AuthenticationSuccessHandler successHandler;
@@ -42,14 +43,13 @@ public class LocalSecurityConfig {
                 )
 
                 .headers(headers -> headers
-                        .frameOptions(frameOptions -> frameOptions.disable())
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
 
                 // 3. 인증되지 않은 사용자 접근 시 (세션 쿠키 없는 경우 포함), 401 에러 반환
                 .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
-                        })
+                        .authenticationEntryPoint((request, response, authException)
+                                -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다."))
                 )
 
                 // 4. OAuth2 로그인은 그대로 유지(선택 사항)
