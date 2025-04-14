503e6d7cfda8d30e526fe633d7e7703b6a22f2ad
박기윤 <qq8918@naver.com>
2025-04-14 19:28:54 +0900

    REFACTORING 파일명 수정

--- src/main/java/com/bonju/review/home/controller/HomeController.java
+++ src/main/java/com/bonju/review/home/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.bonju.review.home.controller;
 import com.bonju.review.knowledge.dto.DayKnowledgeResponseDto;
 import com.bonju.review.quiz.dto.DayQuizResponseDto;
 import com.bonju.review.home.dto.HomeResponseDto;
-import com.bonju.review.knowledge.service.knowledges.KnowledgesService;
+import com.bonju.review.knowledge.service.knowledge_list.KnowledgeListService;
 import com.bonju.review.quiz.service.quizzes.QuizzesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +18,12 @@ import java.util.List;
 public class HomeController {
 
     private final QuizzesService quizzesService;
-    private final KnowledgesService knowledgesService;
+    private final KnowledgeListService knowledgeListService;
 
     @GetMapping
     public HomeResponseDto getQuizzesAndKnowledge() {
         List<DayQuizResponseDto> dayQuizzes = quizzesService.getAllDayQuizzes();
-        List<DayKnowledgeResponseDto> dayKnowledges = knowledgesService.getAllDayKnowledges();
+        List<DayKnowledgeResponseDto> dayKnowledges = knowledgeListService.getAllDayKnowledges();
         return new HomeResponseDto(dayQuizzes, dayKnowledges);
     }
 }
---
+++ src/main/java/com/bonju/review/knowledge/repository/knowledge_list/KnowledgeListRepository.java
@@ -0,0 +1,11 @@
+package com.bonju.review.knowledge.repository.knowledge_list;
+
+import com.bonju.review.knowledge.entity.Knowledge;
+import com.bonju.review.knowledge.vo.SingleDayRange;
+import com.bonju.review.user.entity.User;
+
+import java.util.List;
+
+public interface KnowledgeListRepository {
+    List<Knowledge> findKnowledgeListByDateRange(User user, SingleDayRange dayRange);
+}
