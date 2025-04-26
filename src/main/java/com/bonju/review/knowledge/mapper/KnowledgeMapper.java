e982d31dd48b79a9edda6c3476c18749b3ac2fc1
박기윤 <qq8918@naver.com>
2025-04-26 10:36:25 +0900

    toEntity에서 create 지정하지 않았던 필드 해결

--- src/main/java/com/bonju/review/knowledge/mapper/KnowledgeMapper.java
+++ src/main/java/com/bonju/review/knowledge/mapper/KnowledgeMapper.java
@@ -5,6 +5,8 @@ import com.bonju.review.knowledge.entity.Knowledge;
 import com.bonju.review.user.entity.User;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class KnowledgeMapper {
 
@@ -13,6 +15,7 @@ public class KnowledgeMapper {
                 .user(user)
                 .title(knowledgeRequestDto.getTitle())
                 .content(knowledgeRequestDto.getContent())
+                .createdAt(LocalDateTime.now())
                 .build();
 
 
