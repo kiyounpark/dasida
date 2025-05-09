708b5a5e14825193e0e62be84d7e42337da55962
박기윤 <qq8918@naver.com>
2025-05-09 19:02:03 +0900

    UPDATE KnowledgeResponseDto -> KnowledgeItemResponseDto 로 이름 변

--- src/main/java/com/bonju/review/knowledge/dto/KnowledgeItemResponseDto.java
+++ src/main/java/com/bonju/review/knowledge/dto/KnowledgeItemResponseDto.java
@@ -3,9 +3,12 @@ package com.bonju.review.knowledge.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Getter
-public class KnowledgeResponseDto {
+public class KnowledgeItemResponseDto {
   private Long id;
   private String title;
+  private LocalDateTime createAt;
 }
