2ec82862fb50f6e6c7a4c4c9c07c97149669d2c9
박기윤 <qq8918@naver.com>
2025-05-09 22:08:22 +0900

    Update Service Refactoring

--- src/main/java/com/bonju/review/knowledge/repository/KnowledgeReadRepository.java
+++ src/main/java/com/bonju/review/knowledge/repository/KnowledgeReadRepository.java
@@ -3,7 +3,9 @@ package com.bonju.review.knowledge.repository;
 import com.bonju.review.knowledge.entity.Knowledge;
 import com.bonju.review.user.entity.User;
 
+import java.util.Optional;
+
 public interface KnowledgeReadRepository {
 
-  Knowledge findKnowledge(User user, Long id);
+  Optional<Knowledge> findKnowledge(User user, Long id);
 }
