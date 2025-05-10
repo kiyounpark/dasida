df3566a671b81f903f9375bd8db86d67655c0503
박기윤 <qq8918@naver.com>
2025-05-10 18:22:35 +0900

    Update KnowledgeReadServiceImpl.java 트랜잭션 코드 추가

--- src/main/java/com/bonju/review/knowledge/service/KnowledgeReadServiceImpl.java
+++ src/main/java/com/bonju/review/knowledge/service/KnowledgeReadServiceImpl.java
@@ -6,6 +6,7 @@ import com.bonju.review.knowledge.exception.KnowledgeException;
 import com.bonju.review.knowledge.repository.KnowledgeReadRepository;
 import com.bonju.review.user.entity.User;
 import com.bonju.review.user.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +19,7 @@ public class KnowledgeReadServiceImpl implements KnowledgeReadService{
   private final UserService userService;
 
   @Override
+  @Transactional
   public KnowledgeDetailResponseDto getKnowledgeById(Long id) {
     User user = userService.findUser();
 
