2489cebbca2da6d022bda1ff7d947b76de080c4d
박기윤 <qq8918@naver.com>
2025-05-11 10:54:44 +0900

    ADD TDD KnowledgeSearchService RED

---
+++ src/main/java/com/bonju/review/knowledge/service/KnowledgeSearchServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bonju.review.knowledge.service;
+
+import com.bonju.review.knowledge.dto.KnowledgeSearchResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KnowledgeSearchServiceImpl implements KnowledgeSearchService{
+
+
+
+  @Override
+  public KnowledgeSearchResponseDto searchKnowledgeByTitle(String title) {
+    return null;
+  }
+}
