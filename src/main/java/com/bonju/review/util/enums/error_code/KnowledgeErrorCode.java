1778a4805741ea5fc0780a8d01576ee923fc1a3c
박기윤 <qq8918@naver.com>
2025-05-27 23:25:13 +0900

    지식 등록 서비스 tdd 방식으로 추가

--- src/main/java/com/bonju/review/util/enums/error_code/KnowledgeErrorCode.java
+++ src/main/java/com/bonju/review/util/enums/error_code/KnowledgeErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum KnowledgeErrorCode {
   NOT_FOUND(HttpStatus.NOT_FOUND, "지식을 찾을 수 없습니다"),
-  RETRIEVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "지식 조회 중 오류가 발생했습니다");
-
+  RETRIEVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "지식 조회 중 오류가 발생했습니다"),
+  REGISTER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "지식 DB에 저장중 오류가 발생했습니다");
   private final HttpStatus httpStatus;
   private final String message;
 }
