6d57669aa16e3efd76667a2af605ba28e88b13b3
박기윤 <qq8918@naver.com>
2025-06-09 18:42:37 +0900

    지식등록할때 빌더패턴에 user 가 제외되어 유저와 createAt 기간 추가

--- src/main/java/com/bonju/review/knowledge/service/KnowledgeRegistrationServiceImpl.java
+++ src/main/java/com/bonju/review/knowledge/service/KnowledgeRegistrationServiceImpl.java
@@ -3,25 +3,33 @@ package com.bonju.review.knowledge.service;
 import com.bonju.review.knowledge.entity.Knowledge;
 import com.bonju.review.knowledge.exception.KnowledgeException;
 import com.bonju.review.knowledge.repository.KnowledgeRegistrationRepository;
+import com.bonju.review.user.entity.User;
+import com.bonju.review.user.service.UserService;
 import com.bonju.review.util.enums.error_code.KnowledgeErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class KnowledgeRegistrationServiceImpl implements KnowledgeRegistrationService {
 
   private final KnowledgeRegistrationRepository repository;
+  private final UserService userService;
 
   @Override
   @Transactional
   public Knowledge registerKnowledge(String title, String content) {
     try {
+      User user = userService.findUser();
       Knowledge knowledge = Knowledge.builder()
+              .user(user)
               .title(title)
               .content(content)
+              .createdAt(LocalDateTime.now())
               .build();
       repository.save(knowledge);
       return knowledge;
