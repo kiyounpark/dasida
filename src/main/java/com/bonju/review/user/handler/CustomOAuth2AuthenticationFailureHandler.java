00b2ae470d55b062c4349d35635fcb47d0af64c3
박기윤 <qq8918@naver.com>
2025-06-14 20:10:40 +0900

    로그인 예외 발생시 예외 슬랙에 전송

--- src/main/java/com/bonju/review/user/handler/CustomOAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/bonju/review/user/handler/CustomOAuth2AuthenticationFailureHandler.java
@@ -1,10 +1,12 @@
 package com.bonju.review.user.handler;
 
 import com.bonju.review.exception.ErrorResponse;
+import com.bonju.review.slack.SlackErrorMessageFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -16,15 +18,19 @@ import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomOAuth2AuthenticationFailureHandler implements AuthenticationFailureHandler {
 
     private final ObjectMapper objectMapper; // 스프링이 제공하는 기본 ObjectMapper
+    private final SlackErrorMessageFactory slackErrorMessageFactory;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
 
+        String errorMessage = slackErrorMessageFactory.createErrorMessage(request, exception);
+        log.error(errorMessage);
         final String message = "소셜 로그인 인증에 실패했습니다. 다시 시도해주세요.";
 
         // ErrorResponse 객체 생성 (ErrorResponse.java 참고)
