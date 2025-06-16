3f38bdebdd18efc2db48462cd0f3ef727826b3de
박기윤 <qq8918@naver.com>
2025-06-16 23:54:50 +0900

    ADD DeviceTokenRepository 코드 생성

---
+++ src/main/java/com/bonju/review/devicetoken/repository/DeviceTokenRepository.java
@@ -0,0 +1,12 @@
+package com.bonju.review.devicetoken.repository;
+
+import com.bonju.review.devicetoken.entity.DeviceToken;
+import com.bonju.review.user.entity.User;
+
+import java.util.Optional;
+
+public interface DeviceTokenRepository {
+  Optional<DeviceToken> findByUserIdAndToken(User user, String token);
+
+  DeviceToken save(DeviceToken token);
+}
