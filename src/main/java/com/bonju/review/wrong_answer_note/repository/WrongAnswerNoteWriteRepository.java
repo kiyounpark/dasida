78cd05d5c43e0d6fc9a55dfb5f00a41c349de22a
박기윤 <qq8918@naver.com>
2025-06-16 19:58:04 +0900

    필요없는 오답노트 제

--- src/main/java/com/bonju/review/wrong_answer_note/repository/WrongAnswerNoteWriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bonju.review.wrong_answer_note.repository;
-
-import com.bonju.review.wrong_answer_note.entity.WrongAnswerNote;
-
-public interface WrongAnswerNoteWriteRepository {
-    void save(WrongAnswerNote note);
-}
--- src/main/java/com/bonju/review/wrong_answer_note/service/WrongAnswerNoteWriteService.java
+++
@@ -1,7 +0,0 @@
-package com.bonju.review.wrong_answer_note.service;
-
-import com.bonju.review.useranswer.entity.UserAnswer;
-
-public interface WrongAnswerNoteWriteService {
-    void addWrongAnswer(UserAnswer userAnswer);
-}
