1815a93118a6ef3bd1824c20866cdde5e036879b
박기윤 <qq8918@naver.com>
2025-06-16 21:39:15 +0900

    ADD DeviceToken 엔티티 생

---
+++ src/main/java/com/bonju/review/devicetoken/entity/DeviceToken.java
@@ -0,0 +1,43 @@
+package com.bonju.review.devicetoken.entity;
+
+import com.bonju.review.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeviceToken {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false, length = 255)
+  private String token;
+
+  @Column(nullable = false)
+  private boolean active;
+
+  @Builder
+  private DeviceToken(User user, String token) {
+    this.user = user;
+    this.token = token;
+  }
+
+  public void deactivate() {
+    this.active = false;
+  }
+
+  public void refresh(String newToken) {
+    this.token = newToken;
+    this.active = true;
+  }
+}
