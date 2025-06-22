d0ce73d1b4e1f461bfb66123ca88a63f3940cac6
박기윤 <qq8918@naver.com>
2025-06-22 18:00:27 +0900

    ADD FcmService TDD 로 개

---
+++ src/main/java/com/bonju/review/notification/service/FcmService.java
@@ -0,0 +1,32 @@
+package com.bonju.review.notification.service;
+
+import com.bonju.review.notification.exception.FcmErrorCode;
+import com.bonju.review.notification.exception.FcmException;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FcmService {
+
+  private final FirebaseMessaging firebaseMessaging;
+  public String pushToToken(String token, String title, String body) {
+    Message msg = Message.builder()
+            .setToken(token)
+            .setNotification(Notification.builder()
+                    .setTitle(title)
+                    .setBody(body)
+                    .build())
+            .build();
+    try {
+      return firebaseMessaging.send(msg);
+    } catch (Exception e) {
+      throw new FcmException(FcmErrorCode.PUSH_FAILED, e);
+    }
+  }
+}
