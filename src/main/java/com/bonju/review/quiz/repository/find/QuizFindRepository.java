8255d03388c80830a8169df02f4aa14dc9343622
박기윤 <qq8918@naver.com>
2025-06-22 12:58:43 +0900

    ADD QuizFindRepository.existsQuizByUser TDD

--- src/main/java/com/bonju/review/quiz/repository/find/QuizFindRepository.java
+++ src/main/java/com/bonju/review/quiz/repository/find/QuizFindRepository.java
@@ -1,7 +1,10 @@
 package com.bonju.review.quiz.repository.find;
 
 import com.bonju.review.quiz.entity.Quiz;
+import com.bonju.review.user.entity.User;
 
 public interface QuizFindRepository {
     Quiz findById(Long id);
+
+    boolean existsQuizByUser(User user);
 }
