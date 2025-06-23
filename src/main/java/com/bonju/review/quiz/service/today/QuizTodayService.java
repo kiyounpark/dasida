d10e0cd61aba1ee058f8d2aa5f9588b7df99355f
박기윤 <qq8918@naver.com>
2025-06-23 14:07:23 +0900

    UPDATE QuizTodayService 단위테스트 하나로 통일

--- src/main/java/com/bonju/review/quiz/service/today/QuizTodayService.java
+++ src/main/java/com/bonju/review/quiz/service/today/QuizTodayService.java
@@ -1,25 +1,29 @@
 package com.bonju.review.quiz.service.today;
 
+import com.bonju.review.quiz.entity.Quiz;
 import com.bonju.review.quiz.exception.errorcode.QuizErrorCode;
 import com.bonju.review.quiz.exception.exception.QuizException;
 import com.bonju.review.quiz.repository.QuizTodayRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QuizTodayService {
 
   private final QuizTodayRepository quizTodayRepository;
 
-  public List<Long> findTodayQuizIds(){
-    try{
-      return quizTodayRepository.findTodayQuizIds();
-    }
-    catch (DataAccessException e){
+  /** Quiz 엔티티 + 작성자(User) 모두 */
+  public List<Quiz> findTodayQuizList() {
+    try {
+      List<Long> ids = quizTodayRepository.findTodayQuizIds();
+      return quizTodayRepository.findByIdsWithUser(ids);
+    } catch (DataAccessException e) {
       throw new QuizException(QuizErrorCode.QUIZ_TODAY_FAILED, e);
     }
   }
