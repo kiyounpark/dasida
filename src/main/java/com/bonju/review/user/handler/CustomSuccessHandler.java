b1a5245a10c44d878ecc2c3ada09ef5c80b4d0ae
박기윤 <qq8918@naver.com>
2025-07-13 15:47:48 +0900

    ADD SuccessHandler 리다이렉트 url 수정

--- src/main/java/com/bonju/review/user/handler/CustomSuccessHandler.java
+++ src/main/java/com/bonju/review/user/handler/CustomSuccessHandler.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
