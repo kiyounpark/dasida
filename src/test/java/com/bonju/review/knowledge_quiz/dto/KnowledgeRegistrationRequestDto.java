e82a4730cab84f89c0242881e815635ace7631bd
박기윤 <qq8918@naver.com>
2025-10-27 23:06:50 +0900

    chore: 사용 안하게된 MarkdownLength.java 애노테이션 관려 코드들 제

--- src/test/java/com/bonju/review/knowledge_quiz/dto/KnowledgeRegistrationRequestDto.java
+++ src/test/java/com/bonju/review/knowledge_quiz/dto/KnowledgeRegistrationRequestDto.java
@@ -1,9 +1,8 @@
 package com.bonju.review.knowledge_quiz.dto;
 
-import com.bonju.review.util.annotation.MarkdownLength;
 import jakarta.validation.constraints.NotBlank;
 
 public record KnowledgeRegistrationRequestDto(
         @NotBlank String title,
-        @NotBlank @MarkdownLength(max = 500) String content
+        @NotBlank String content
 ) {}
